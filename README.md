cd1e2009ce13c170a451168d378b85ffe971c4b9
Henrique Santos Silva <henriqueesquecemuito@gmail.com>
2026-04-12 16:52:13 -0300

    All

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-## Repositório destinado a armazenar informações do nosso projeto para o PAC 2026-1
+### Repositório destinado a armazenar informações do nosso projeto para o PAC 2026-1
+
+## Ordem de testes
+
+# Motores
+
+# Sensores
+
+# Linha + motores
+
+# Ultrassônico
+
+# Garra
+
+# Sistema completo
--- Robo/Robo.ino
+++ Robo/Robo.ino
@@ -15,10 +15,10 @@ enum Movimento {
 void setup() {
   Serial.begin(9600);
 
-  motores_init(4,5,6,7);
-  sensores_init(2,3,11,12);
-  ultra_init(8,9);
-  garra_init(10);
+  motores_init(4,5,6,7); // (int eA, int eR, int dA, int dR)
+  sensores_init(2,3,11,12); // (int eExt, int eInt, int dInt, int dExt)
+  ultra_init(8,9); // int trig, int echo
+  garra_init(10); // int pino
 }
 
 Movimento calcular(bool eExt, bool eInt, bool dInt, bool dExt) {
@@ -30,7 +30,7 @@ Movimento calcular(bool eExt, bool eInt, bool dInt, bool dExt) {
 
 void loop() {
   bool eExt, eInt, dInt, dExt;
-  lerSensores(eExt, eInt, dInt, dExt);
+  lerSensores(eExt, eInt, dInt, dExt); // (bool &eExt, bool &eInt, bool &dInt, bool &dExt)
 
   Movimento m = calcular(eExt, eInt, dInt, dExt);
 
